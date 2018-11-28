69864948d64eaa19450d276017aa2765be3d9a37
zhangwei <551565555@qq.com>
2018-11-28 17:45:29 +0800

    zw
    成功对所有的一级标签进行点击操作

--- autohome_splider/spiders/AutoHomeSplider.py
+++ autohome_splider/spiders/AutoHomeSplider.py
@@ -1,6 +1,9 @@
+import time
 from telnetlib import EC
 
 import scrapy
+from selenium.webdriver.support.select import Select
+
 from autohome_splider.items import AutohomeSpliderItem
 from selenium import webdriver
 import selenium.webdriver.support.ui as ui
@@ -21,20 +24,28 @@ class AutoHomeSplider(scrapy.Spider):
         # wait = ui.WebDriverWait(driver, 10)
         driver.get("https://www.autohome.com.cn/shenzhen/")
         driver.implicitly_wait(10)
-        # wait.until(lambda driver: driver.find_element_by_xpath("//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']/div[@class='brand eye-protector-processed']/dl/dd"))
-        selectBar = driver.find_element_by_xpath("/html[@class='eye-protector-processed']/body/div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__selected']").extract()
-        print(selectBar)
-        # brandList = driver.find_element_by_xpath("//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']/div[@class='brand eye-protector-processed']/dl/dd").extract()
-        # print(brandList)
-        # for i_brand in brandList:
-        #     i_brandName = driver.find_element_by_xpath("//div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__selected']").extract().strip()
-        #     print(i_brandName)
-
-        # car_data_values = response.xpath("//div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']/div[@class='series eye-protector-processed show-animate']/dl[@class='list']/dd[2]/@data-value").extract()
-        #
-        # for i_data_value in car_data_values:
-        #     print(i_data_value)
-
+        #定位到汽车之家首页的下拉框处 并触发点击事件
+        driver.find_element_by_xpath("//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__selected']/span[@class='athm-select__icon']/i[@class='athm-iconfont athm-iconfont-arrowdown']").click()
+        #获取点击下拉框之后出现的下拉框值
+        select = driver.find_element_by_xpath("//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']//div[1]")
+        # print(driver.page_source)
+        #获取所有的dd元素  （品牌信息包含在里面）
+        options = select.find_elements_by_tag_name("dd")
+        i = 1
+        for i_option in options:
+            print(i_option)
+            #拼接一级标签的xpath路径
+            i_path = "//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']//div[1]/dl/dd[" + str(i)+ "]"
+            i += 1
+            print(i_path)
+            #根据拿到的一级标签的xpath路径进行点击操作  以便获取二级标签的内容
+            select.find_element_by_xpath(i_path).click()
+            print(i_option.text)
+            # print(i_option.get_attribute("data-value"))
+        # selectValues = driver.find_element_by_xpath("//div[@class='wrapper']/div[@class='homepage-findcar']/div[@class='findcar-select']/div[@class='option option-brand']/div[@id='js-carpicker-brand1']/div[@class='athm-select__option']/div[@class='pop-wrapper']/div[@class='brand eye-protector-processed']/dl/dd")
+        # sel = Select(selectValues)
+        # allOptions = sel.options
+        # print(allOptions)
         # print(response.text)
 
     def waitClick(self, time, element):
